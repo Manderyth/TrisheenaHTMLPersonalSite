ad4e7d987cbcc11cfd76a924b8b41227c77ed955
trisheenahepting <manderyth@yahoo.com>
2016-05-22 11:28:10 -0600

    5/21/16 11:28pm

--- GolfCard.js
+++ GolfCard.js
@@ -1,47 +1,54 @@
-/* add this
-for (var c = numholes; c >= 1; c --) {
-    holecollection += "<div id='column" + c"' class'holecol'><div class 'holenumbertitle'>" + c + "</div></div>";
+var numplayers = 8;
+var numholes = 18;
+var teetime = 10;
+var seconds = 59;
+
+function buildcard(){
+    beginTimer();
+    var holecollection = "";
+    var playercollection = "";
+
+    // create column of player labels
+    for(var pl = 1; pl <= numplayers; pl++ ){
+        playercollection += "<div id='player" + pl +"' class='holebox playerbox'> Player " + pl + "</div>";
+    }
+
+    // create golf hole columns before you add holes to them.
+    for(var c = numholes; c >= 1; c-- ){
+        holecollection += "<div id='column" + c +"' class='holecol'><div class='holenumbertitle'>" + c + "</div></div>";
+    }
+    $("#leftcard").html(playercollection);
+    $("#rightcard").html(holecollection);
+
+    // call the function that builds the holes into the columns
+    buildholes();
 }
-$("#leftcard").html(playercollection;
-$("rightcard").holecollection); */
 
-
-/* create a div on the left that holds the number of players
-
-create a new div that creates the columns on the right - put this before you run the column that adds the holes
-
-add $("#column + h ") 
- */
-
-
-/*var myVar = setInterval(function) {myTimer()}, 1000;
-
-function myTimer() {
-    var d = new Date();
-    var t = d.toLocaleTimeString();
-    document.getElementById("demo").innerHTML = t;
+function buildholes() {
+    // add 18 holes to the columns
+    for(var p = 1; p <= numplayers; p++ ){
+        for(var h = 1; h <= numholes; h++){
+            $("#column" + h).append("<div id='player" + p +"hole" + h +"' class='holebox'><input class ='box' type='number'></div>");
+        }
+    }
 }
 
-function myStopFunction (
-    clearinterval(myVar);
-) */
-
-
-var teetime = 45;
-var seconds = 60;
-
-function beginTimer() {    //call this onload
+function beginTimer(){
     var thetimer = setInterval(function(){clocktick()}, 1000);
 }
 
-function clocktick() {
-    if(seconds > 0) {
-        seconds--;
+function clocktick(){
+    if(seconds > 0){
+        seconds --;
+        if(seconds < 10){
+            seconds = "0" + seconds;
+        }
     }
-    
-    else {
+    else{
         teetime --;
-        seconds = 60;
+        seconds = 59;
     }
-        document.getElementById("countdown").innerHTML = teetime + ":" + seconds;
+    document.getElementById("countdown").innerHTML = teetime + ":" + seconds;
 }
+
+
