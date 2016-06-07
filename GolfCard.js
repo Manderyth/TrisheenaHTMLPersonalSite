f1ad3d5aa2532bcb456be0a12e3389d981a7ccbd
trisheenahepting <manderyth@yahoo.com>
2016-06-07 11:07:07 -0600

    6/6/16 11:07am

--- GolfCard.js
+++ GolfCard.js
@@ -1,18 +1,20 @@
-var numplayers = 4;
+var numplayers = 0;
 var numholes = 18;
 var teetime = 10;
 var seconds = 59;
-
+var response = [];
 
 function addplayer() {
     numplayers += 1;
     //$("#leftcard").html('');
     //$("#rightcard").html('');
-    buildcard();
+    teeboxid=$("#selectteebox").val();
+    buildcard(teeboxid);
     getCourseInfo();
+
 }
 
-function buildcard() {
+function buildcard(teeboxid) {
     /*beginTimer();*/
     var holecollection = "";
     var playercollection = "";
@@ -20,9 +22,10 @@ function buildcard() {
     var firstnine="";
     var secondnine="";
 
+
     // create column of player labels
     for (var pl = 1; pl <= numplayers; pl++) {
-        playercollection += "<div id='player" + pl + "' class='holebox playerbox'>Player " + pl + "<input id='name'></div>";
+        playercollection += "<div id='player" + pl + "' class='holebox playerbox'><input id='name'> Player " + pl + "</div>";
         grandtotalcollection += "<div class='holebox' id='grand" + pl +"'>0</div>";
         firstnine += "<div class='holebox' id='f9" + pl +"'>0</div>";
         secondnine += "<div class='holebox' id='s9" + pl +"'>0</div>";
@@ -31,8 +34,13 @@ function buildcard() {
 
     // create golf hole columns before you add holes to them.
     for (var c = numholes; c >= 1; c--) {
-        holecollection += "<div id='column" + c + "' class='holecol'><div class='holenumbertitle'>" + c  + "</div></div>";
+        var adjusthole = c - 1;
+        var adjusthole2 = c - 1;
+        var adjusthole3 = c - 1;
+        holecollection += "<div id='column" + c + "' class='holecol'><div class='holenumbertitle'><div>" + c + '<div> Par ' + (response.course.holes[adjusthole].tee_boxes[teeboxid].par) + '<div> Yards ' + (response.course.holes[adjusthole2].tee_boxes[teeboxid].yards) + '<div> H ' + (response.course.holes[adjusthole2].tee_boxes[teeboxid].hcp) +"</div></div></div></div></div></div>";
     }
+
+
     $("#leftcard").html(playercollection);
     $("#rightcard").html(("<div class='holecol'><div>total</div>" + grandtotalcollection + "</div>") + ("<div class='holecol'><div> 2nd9Total</div>" + secondnine + "</div>") + ("<div class='holecol'><div> 1st9Total</div>" + firstnine + "</div>") + holecollection);
 
@@ -42,14 +50,36 @@ function buildcard() {
 }
 
 
+
+
 function buildholes() {
     // add 18 holes to the columns
     for (var p = 1; p <= numplayers; p++) {
         for (var h = 1; h <= numholes; h++) {
-            $("#column" + h).append("<input onkeyup='calculatescore(" + p +")' id='player" + p + "hole" + h + "' class='holebox'/>");
+            $("#column" + h).append("<input onkeyup='calculatescore(" + p + ")' id='player" + p + "hole" + h + "' class='holebox'/>");
         }
     }
+    validatescores();
 }
+
+
+
+
+function validatescores() {
+    var x, text;
+
+    // Get the value of the input field with id=""
+    x = document.getElementsByClassName("holebox").value;
+
+    // If x is Not a Number or less than one or greater than 10
+    if (isNaN(x) || x < 1 || x > 10) {
+        text = "Input not valid";
+    } else {
+        text = "Input OK";
+    }
+}
+
+
 
 
 function calculatescore(theplayer) {
@@ -58,6 +88,8 @@ function calculatescore(theplayer) {
         thetotal += Number($("#player" + theplayer + "hole" + t).val());
     }
     $("#grand" + theplayer).html(thetotal);
+    $("#s9" + theplayer).html(thetotal);
+    $("#f9" + theplayer).html(thetotal);
 }
 
 
@@ -144,10 +176,16 @@ function getCourseInfo() {
     var xhttpNew = new XMLHttpRequest();
     xhttpNew.onreadystatechange = function () {
         if (xhttpNew.readyState == 4 && xhttpNew.status == 200) {
-            var response = JSON.parse(xhttpNew.responseText);
-            for (var r = 0; r <= numholes; r++) {
-                $("#column" + (r + 1)).append("<span> Par </span><span class=''>" + response.course.holes[r].tee_boxes[3].par + "</span><br>");
-                $("#column" + (r + 1)).append("<span> Yards </span><span class=''>" + response.course.holes[r].tee_boxes[3].yards + "</span>");
+            response = JSON.parse(xhttpNew.responseText);
+            for (var r = 0; r <= (response.course.holes[r].tee_boxes.length - 1); r++) {
+                var teeboxdisplay = "<option value='" + r + "'> "+ response.course.holes[0].tee_boxes[r].tee_type +"</option>";
+                var teeboxdisplay2 = "<option value='" + r + "'> "+ response.course.holes[0].tee_boxes[r].yards +"</option>";
+                var teeboxdisplay3 = "<option value='" + r + "'> "+ response.course.holes[0].tee_boxes[r].hcp +"</option>";
+                $("#selectteebox").append(teeboxdisplay, teeboxdisplay2, teeboxdisplay3);
+
+
+                /*$("#column" + (r + 1)).append("<span> Par </span><span class=''>" + response.course.holes[r].tee_boxes[3].par + "</span><br>");
+                 $("#column" + (r + 1)).append("<span class=''>" + response.course.holes[r].tee_boxes[3].yards + "</span><span> Yards </span>");*/
             }
         }
     };
@@ -156,3 +194,9 @@ function getCourseInfo() {
 };
 
 
+var teeboxid;
+
+function setCourseinfo (thisid) {
+    teeboxid=thisid;
+    buildcard(thisid);
+}
